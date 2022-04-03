2f07b7e481d48bcb653ef1a0c73b18e59d1571c0
mhd <mhdhd@gmail.com>
2022-04-03 21:56:28 +0400

    creating create_classrom.js endpoint

--- src/routes/classroom/create_classrom.js
+++ src/routes/classroom/create_classrom.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const {authorizeForInstructor} = require('../../middleware/autherization')
-const jwt = require("jsonwebtoken");
-/* GET home page. */
-router.post('/create',authorizeForInstructor,function(req, res, next) {
-    res.json({ user_id: req.user_id});
+const {authorizeForInstructor} = require('../../middleware/autherization');
+const {Classroom} = require("../../models/Classroom");
+
+router.post('/create',authorizeForInstructor,async function (req, res, next) {
+    const {class_name} = req.body;
+    const [classroom, created] = await Classroom.findOrCreate({
+        where: {instructor_id : req.user_id, class_name },
+        defaults: {
+            instructor_id : req.user_id,
+            class_name
+        }
+    })
+    if (created){
+        res.json({message: `you already have classroom named ${class_name}`});
+    }else {
+        res.json({message: `classroom created successfully!`});
+    }
+
 });
 
 module.exports = router;
